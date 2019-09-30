3eeb2b71ad404844456af8dde27bdad29cda0e2b
Jeel Patel <jeelpatel007@yahoo.com>
2019-09-29 20:39:04 -0400

    Revert "Added Maven support"
    This reverts commit 1ee14a2b8cb07050b1826d25ea12cc501a46cd1e.

--- bubblesLaundry/src/com/bubblesLaundry/servlet/handler/LoginValidation.java
+++ bubblesLaundry/src/com/bubblesLaundry/servlet/handler/LoginValidation.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,7 @@ import com.bubblesLaundry.dao.StoreDAO;
 /**
  * Servlet implementation class LoginValidation
  */
+@WebServlet("/LoginValidation")
 public class LoginValidation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- bubblesLaundry/src/com/bubblesLaundry/servlet/handler/StoreHandler.java
+++ bubblesLaundry/src/com/bubblesLaundry/servlet/handler/StoreHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import com.bubblesLaundry.dao.StoreDAO;
 /**
  * Servlet implementation class StoreHandler
  */
+@WebServlet("/StoreHandler")
 public class StoreHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String INSERT = "/AddStore.jsp";
